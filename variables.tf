fd8961247a0537ff2d28729940e8b64bc74b268d
Harika-Ponna <Harika.Ponna@ibm.com>
2026-01-20 13:42:56 +0530

    fix: update desc for var

--- variables.tf
+++ variables.tf
@@ -272,6 +272,6 @@ variable "schematics_policies" {
 variable "install_required_binaries" {
   type        = bool
   default     = true
-  description = "When set to true, a script will run to check if `kubectl` and `jq` exist on the runtime and if not attempt to download them from the public internet and install them to /tmp. Set to false to skip running this script."
+  description = "When set to true, a script will run to check if `jq` exist on the runtime and if not attempt to download them from the public internet and install them to /tmp. Set to false to skip running this script."
   nullable    = false
 }
