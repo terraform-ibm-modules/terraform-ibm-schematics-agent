0be8384aa87facfa64089d3bd8a6cd8f1705ddb4
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-06-08 19:13:58 +0530

    chore(deps): update dependencies (#160)

--- examples/openshift/main.tf
+++ examples/openshift/main.tf
@@ -80,7 +80,7 @@ locals {
 
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.87.3"
+  version              = "3.87.4"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   tags                 = var.resource_tags
