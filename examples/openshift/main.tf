701d0fe269d428b4453be3ffcd8465a1bc423e64
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-09-01 14:11:15 +0530

    chore(deps): update dependencies (#170)
    Co-authored-by: Md Anam Raihan <md.anam.raihan17@gmail.com>

--- examples/openshift/main.tf
+++ examples/openshift/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.17.7"
+  version                = "10.17.8"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
@@ -80,7 +80,7 @@ locals {
 
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.90.4"
+  version              = "3.91.0"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   resource_tags        = var.resource_tags
